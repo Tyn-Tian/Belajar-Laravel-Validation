daf1b475120b1302c69113ac298b02c513417195
Christian <kristian2015ok@gmail.com>
2024-05-30 15:40:50 +0700

    Add: Validation Message

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
@@ -77,6 +78,8 @@ class ValidatorTest extends TestCase
 
     public function testValidatorMultipleRules()
     {
+        App::setLocale("id");
+
         $data = [
             "username" => "tian",
             "password" => "tian"
@@ -120,6 +123,40 @@ class ValidatorTest extends TestCase
         try {
             $valid = $validator->validate();
             Log::info(json_encode($valid, JSON_PRETTY_PRINT));
+        } catch(ValidationException $exception) {
+            self::assertNotNull($exception);
+            $message = $exception->validator->errors();
+            Log::error($message->toJson(JSON_PRETTY_PRINT));
+        }
+    }
+
+    public function testValidatorInlineMessage()
+    {
+        App::setLocale("id");
+
+        $data = [
+            "username" => "tian",
+            "password" => "tian"
+        ];
+
+        $rules = [
+            "username" => "required|email|max:100",
+            "password" => ["required", "min:6", "max:20"],
+        ];
+
+        $message = [
+            "required" => ":attribute harus diisi",
+            "email" => ":attribute harus berupa email",
+            "min" => ":attribute minimal :min karakter",
+            "max" => ":attribute maksimal :max karakter",
+        ];
+
+        $validator = Validator::make($data, $rules, $message);
+        self::assertNotNull($validator);
+        
+        try {
+            $validator->validate();
+            self::fail("ValidationException not thrown");
         } catch(ValidationException $exception) {
             self::assertNotNull($exception);
             $message = $exception->validator->errors();
