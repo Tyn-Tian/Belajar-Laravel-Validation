191bd0e00621ccba8959fac28472bbb956cb7a20
Christian <kristian2015ok@gmail.com>
2024-05-30 21:36:47 +0700

    Add: Rule Classes

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -10,6 +10,8 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\In;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Validation\Validator as ValidationValidator;
 use Tests\TestCase;
@@ -238,6 +240,29 @@ class ValidatorTest extends TestCase
                 }
             }],
             "password" => ["required", "min:6", "max:20", new RegistrationRule()]
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        self::assertFalse($validator->passes());
+        self::assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function testValidatorRuleClasses()
+    {
+        $data = [
+            "username" => "tian@gmail.com",
+            "password" => "tian@gmail.com"
+        ];
+
+        $rules = [
+            "username" => ["required", new In(["Chris", "Tian", "TYN"])],
+            "password" => ["required", Password::min(6)->letters()->numbers()->symbols()]
         ];
 
         $validator = Validator::make($data, $rules);
