2d254d8f2301b119145b991007328129db76d7b7
Christian <kristian2015ok@gmail.com>
2024-05-30 14:53:20 +0700

    Add: Error Message

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
@@ -43,5 +44,8 @@ class ValidatorTest extends TestCase
         self::assertNotNull($validator);
         self::assertFalse($validator->passes());
         self::assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 }
